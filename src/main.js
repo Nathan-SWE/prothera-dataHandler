e4224efdde2879a43c8dbdc8c6fab5d68508b5c6
Nathan Lima <nathanls.swe@gmail.com>
2025-04-01 23:47:58 -0300

    chore: renomeia dir 'assets' para 'src'

---
+++ src/main.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "../data/data.json");
+const searchID = 2;
+const data = loadJSONData(filePath);
+
+function loadJSONData(filePath) {
+  try {
+    const jsonData = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.log("Error when loading json file:", error);
+    return null;
+  }
+}
+
+function findUserByID(data, id) {
+  return data.find((user) => user.id === id);
+}
+
+if (data) {
+  const user = findUserByID(data, searchID);
+  console.log(user ? "\nUser found:" : "\nUser not found:", user);
+}
+
+function getCPF(user) {
+  const docCPF = user.documents.find((doc) => doc.type === "CPF");
+  return docCPF ? docCPF.number : null;
+}
+
+function listUserByAge(data) {
+  return data
+    .filter((user) => getCPF(user) !== null)
+    .sort((a, b) => a.age - b.age)
+    .map((user) => ({
+      Name: user.name,
+      Age: user.age,
+      CPF: getCPF(user),
+    }));
+}
+
+if (data) {
+  const sortedUsers = listUserByAge(data);
+  console.log("\nPeople with CPF ordered by age:", sortedUsers);
+}
+
+function usersAgeGreaterThan50(data) {
+  return data
+    .filter((user) => user.age > 50)
+    .map((user) => ({
+      ID: user.id,
+      Name: user.name,
+      Age: user.age,
+    }));
+}
+
+if (data) {
+  const elders = usersAgeGreaterThan50(data);
+  console.log("\nPeople over 50 years old:", elders);
+}
+
+function listUsersWithoutCPF(data) {
+  return data.filter((user) => !getCPF(user));
+}
+
+const peopleWithoutCPF = listUsersWithoutCPF(data);
+console.log("\nUsers without CPF:", peopleWithoutCPF);
+
+function listAllDocumentTypes(data) {
+  const documentTypes = [
+    ...new Set(data.flatMap((user) => user.documents.map((doc) => doc.type))),
+  ];
+  return documentTypes;
+}
+
+const allDocumentTypes = listAllDocumentTypes(data);
+console.log("\nAll document types:", allDocumentTypes);
