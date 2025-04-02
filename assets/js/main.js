93fba1a0d222f25d0869f3a362c30e371e77804c
Nathan Lima <nathanls.swe@gmail.com>
2025-04-01 21:57:11 -0300

    refactor: cria funcao com responsabilidade de carregar os dados do .json

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,12 @@ const path = require("path");
 
 const filePath = path.join(__dirname, "../../data/data.json");
 
-try {
-  const jsonData = fs.readFileSync(filePath, "utf8");
-  const data = JSON.parse(jsonData);
-  console.log("Dados: carregados:", data);
-} catch (error) {
-  console.log("Erro ao carregar arquivo JSON:", error);
+function loadJSONData(filePath) {
+  try {
+    const jsonData = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.log("Erro ao carregar arquivo JSON:", error);
+    return null;
+  }
 }
